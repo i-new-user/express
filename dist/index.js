6ef4767412e9259c3268d4e090d9fa1a49ec9b20
Stanislav <gsv9026@mail.ru>
2023-02-08 09:14:29 +0300

    _______

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 3003;
+const port = 3005;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 let videos = [
@@ -57,18 +57,54 @@ const error = { "errorsMessages": [{
 app.get('/', (req, res) => {
     res.send('<h1>Hello world !!! >>>>>>>><h1>');
 });
+app.delete('/testing/all-data', (req, res) => {
+    videos.splice(0, videos.length);
+    res.status(HTTP_STATUSES.NO_CONTENT_204);
+});
 app.get('/videos', (req, res) => {
     res.status(HTTP_STATUSES.OK_200).json(videos);
+});
+app.post('/videos', (req, res) => {
+    let title = req.body.title;
+    let author = req.body.author;
+    let availableResolutions = req.body.availableResolutions;
+    let newVideo = {
+        id: +(new Date),
+        title: title,
+        author: author,
+        canBeDownloaded: req.body.canBeDownloaded || true,
+        minAgeRestriction: req.body.minAgeRestriction || null,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date(new Date().setDate(new Date().getDate() + 1)).toISOString(),
+        availableResolutions: req.body.availableResolutions || ['P144'],
+    };
+    if (!title || title.length > 40 || typeof title !== 'string' ||
+        !author || author.length > 20 || typeof author !== 'string' ||
+        !availableResolutions || availableResolutions.length < 1) {
+        res.status(HTTP_STATUSES.BAD_REQUST_400).json(error);
+    }
+    else {
+        videos.push(newVideo);
+        res.status(HTTP_STATUSES.CREATED_201).json(newVideo);
+    }
 });
 app.get('/videos/:id', (req, res) => {
     let video = videos.find(v => v.id === +req.params.id);
     !video
-        ? res.status(HTTP_STATUSES.NOT_FOUND_404).json(error)
+        ? res.status(HTTP_STATUSES.NOT_FOUND_404)
         : res.status(HTTP_STATUSES.OK_200).json(video);
 });
-app.delete('/testing/all-data', (req, res) => {
-    videos = [];
-    res.status(HTTP_STATUSES.NO_CONTENT_204);
+app.put('/videos', (req, res) => {
+});
+app.delete('/videos/:id', (req, res) => {
+    for (let i = 0; i < videos.length; i++) {
+        if (videos[i].id === +req.params.id) {
+            videos.splice(i, 1);
+            res.status(HTTP_STATUSES.NO_CONTENT_204);
+            return;
+        }
+    }
+    res.status(HTTP_STATUSES.NOT_FOUND_404);
 });
 app.listen(port, () => {
     console.log('Start');
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,23 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3003;
+const port = 3005;
 
 app.use(cors())
 app.use(bodyParser.json())
 
+export type VideosType = {
+    id: number
+    title: string
+    author: string
+    canBeDownloaded: boolean
+    createdAt: string
+    publicationDate: string
+    availableResolution: Array<ResolutionsType>
+}
 
+export type ResolutionsType = Array<string>
+    
 let videos = [
     {
         id: 1,
@@ -32,7 +43,7 @@ let videos = [
    },
    {
         id: 3,
-        title: "nature",
+        title: "Nature",
         author: "it-incubator",
         canBeDownloaded: false,
         minAgeRestriction: null,
@@ -62,22 +73,71 @@ app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Hello world !!! >>>>>>>><h1>')
 })
 
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    videos.splice(0, videos.length);
+    res.status(HTTP_STATUSES.NO_CONTENT_204)
+   
+})
+
 app.get('/videos', (req: Request, res: Response) => {
     res.status(HTTP_STATUSES.OK_200).json(videos)
+})
+
+app.post('/videos', (req: Request, res: Response) => {
+    
+    let title = req.body.title;
+    let author = req.body.author;
+    let availableResolutions = req.body.availableResolutions;
+
+    let newVideo ={
+        id: +(new Date),
+        title: title,
+        author: author,
+        canBeDownloaded: req.body.canBeDownloaded || true,
+        minAgeRestriction: req.body.minAgeRestriction || null,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date( new Date().setDate(new Date().getDate() + 1) ).toISOString(),
+        availableResolutions: req.body.availableResolutions || ['P144'],
+    }
+    
+    if(!title || title.length > 40 || typeof title !== 'string' ||
+       !author || author.length > 20 || typeof author !== 'string' ||
+       !availableResolutions || availableResolutions.length < 1){
+
+        res.status(HTTP_STATUSES.BAD_REQUST_400).json(error)
+    } else {
+        videos.push(newVideo);
+        res.status(HTTP_STATUSES.CREATED_201).json(newVideo)
+    }
 })
 
 app.get('/videos/:id', (req: Request, res: Response) => {
     let video = videos.find(v => v.id === +req.params.id)
   
     !video 
-    ? res.status(HTTP_STATUSES.NOT_FOUND_404).json(error)
+    ? res.status(HTTP_STATUSES.NOT_FOUND_404)
     : res.status(HTTP_STATUSES.OK_200).json(video)
 })
 
-app.delete('/testing/all-data', (req: Request, res: Response) => {
-    videos = [];
-    res.status(HTTP_STATUSES.NO_CONTENT_204)
+
+app.put('/videos', (req: Request, res: Response) => {
+   
 })
+
+app.delete('/videos/:id', (req: Request, res: Response) => {
+
+    for(let i = 0; i < videos.length; i++){
+        if(videos[i].id === +req.params.id){
+            videos.splice(i,1)
+            res.status(HTTP_STATUSES.NO_CONTENT_204)
+            return;
+        }
+    }
+    res.status(HTTP_STATUSES.NOT_FOUND_404)
+})
+
+
 
 app.listen(port, () => {
     console.log('Start')
