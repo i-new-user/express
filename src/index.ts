2a468dca880d5bf0980d19c156304994988d3658
Stanislav <gsv9026@mail.ru>
2023-02-17 12:22:59 +0300

    -----

--- src/index.ts
+++ src/index.ts
@@ -25,20 +25,9 @@ export type VideosType = {
     availableResolution: Array<ResolutionsType>
 }
 
+
 export type ResolutionsType = Array<string>
-    
-export let videos = [
-    {
-        id: 1,
-        title: "Ocean",
-        author: "it-incubator",
-        canBeDownloaded: false,
-        minAgeRestriction:  null,
-        createdAt: new Date().toISOString(),
-        publicationDate: new Date( new Date().setDate(new Date().getDate() + 1) ).toISOString(),
-        availableResolutions: ["P144"]
-     }
-]
+
 
 export const HTTP_STATUSES = {
     OK_200: 200,
@@ -54,6 +43,20 @@ export const error = {"errorsMessages":[{
      "field": "title"
  }]
 }
+    
+export let videos = [
+    {
+        id: 1,
+        title: "Ocean",
+        author: "it-incubator",
+        canBeDownloaded: false,
+        minAgeRestriction:  null,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date( new Date().setDate(new Date().getDate() + 1) ).toISOString(),
+        availableResolutions: ["P144"]
+     }
+]
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello')
@@ -66,75 +69,183 @@ app.delete('/testing/all-data', (req: Request, res: Response) => {
 })
 
 app.get('/videos', (req: Request, res: Response) => {
-    res.sendStatus(HTTP_STATUSES.OK_200).send(videos)
+    res.send(videos).sendStatus(HTTP_STATUSES.OK_200)
 })
 
-app.post('/videos', (req: Request, res: Response) => {
-    
-    let title = req.body.title;
-    let author = req.body.author;
-    let availableResolutions = req.body.availableResolutions;
+// app.get('/videos/:id', (req: Request, res: Response) => {
+//     let video = videos.find(v => v.id === +req.params.id)
+//     video ?
+//     res.send(video).sendStatus(HTTP_STATUSES.OK_200) :
+//     res.send(error).sendStatus(HTTP_STATUSES.NOT_FOUND_404) ;
+// })
 
-    let newVideo ={
-        id: +(new Date),
-        title: title,
-        author: author,
-        canBeDownloaded: req.body.canBeDownloaded || true,
-        minAgeRestriction: req.body.minAgeRestriction || null,
-        createdAt: new Date().toISOString(),
-        publicationDate: new Date( new Date().setDate(new Date().getDate() + 1) ).toISOString(),
-        availableResolutions: req.body.availableResolutions || ['P144'],
-    }
-    
-    if(!title || title.length > 40 || typeof title !== 'string' ||
-       !author || author.length > 20 || typeof author !== 'string'){
-
-        res.sendStatus(HTTP_STATUSES.BAD_REQUST_400).send(error)
-    } else {
-        videos.push(newVideo);
-        res.sendStatus(HTTP_STATUSES.CREATED_201).send(newVideo)
-    }
-})
-
-app.get('/videos/:id', (req: Request, res: Response) => {
-    let video = videos.find(v => v.id === +req.params.id)
-  
-    video 
-    ? res.sendStatus(HTTP_STATUSES.OK_200).send(video) 
-    : res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-    
-})
+// !video || !String(video).trim() || (video.length < 10 && video.length > 40)
 
 
-app.put('/videos/:id', (req: Request, res: Response) => {
-    
-   let video = videos.find(v => v.id === +req.params.id)
-   
-  
-   
-   if(video){
-         video.title = req.body.title;
-         video.author = req.body.author;
-        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-   } else {
-          res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-   } 
-})
 
-app.delete('/videos/:id', (req: Request, res: Response) => {
 
-    for(let i = 0; i < videos.length; i++){
-        if(videos[i].id === +req.params.id){
-            videos.splice(i,1)
-            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-            return;
-        }
-    }
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-})
+
+
+
+
+
+
+
+
 
 
 
 app.listen(port, () => {
     console.log(`Start port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post('/videos', (req: Request, res: Response) => {
+    
+//     let title = req.body.title;
+//     let author = req.body.author;
+//     let availableResolutions = req.body.availableResolutions;
+
+//     let newVideo ={
+//         id: +(new Date),
+//         title: title,
+//         author: author,
+//         canBeDownloaded: req.body.canBeDownloaded || true,
+//         minAgeRestriction: req.body.minAgeRestriction || null,
+//         createdAt: new Date().toISOString(),
+//         publicationDate: new Date( new Date().setDate(new Date().getDate() + 1) ).toISOString(),
+//         availableResolutions: req.body.availableResolutions || ['P144'],
+//     }
+    
+//     if(!title || title.length > 40 || typeof title !== 'string' ||
+//        !author || author.length > 20 || typeof author !== 'string'){
+
+//         res.sendStatus(HTTP_STATUSES.BAD_REQUST_400).send(error)
+//     } else {
+//         videos.push(newVideo);
+//         res.sendStatus(HTTP_STATUSES.CREATED_201).send(newVideo)
+//     }
+// })
+
+// app.get('/videos/:id', (req: Request, res: Response) => {
+//     let video = videos.find(v => v.id === +req.params.id)
+  
+//     video 
+//     ? res.sendStatus(HTTP_STATUSES.OK_200).send(video) 
+//     : res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    
+// })
+
+
+// app.put('/videos/:id', (req: Request, res: Response) => {
+    
+//    let video = videos.find(v => v.id === +req.params.id)
+   
+  
+   
+//    if(video){
+//          video.title = req.body.title;
+//          video.author = req.body.author;
+//         res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+//    } else {
+//           res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+//    } 
+// })
+
+// app.delete('/videos/:id', (req: Request, res: Response) => {
+
+//     for(let i = 0; i < videos.length; i++){
+//         if(videos[i].id === +req.params.id){
+//             videos.splice(i,1)
+//             res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+//             return;
+//         }
+//     }
+//     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+// })
+
